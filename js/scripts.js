ecc304eb1ecb505594ea27dd90a544988a648263
Renato Pérez <rendan1011@gmail.com>
2021-02-20 12:21:01 +0100

    Cantidad en los colores

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let num1 = 255;
+let num2 = 111;
+let num3 = 100;
+colores(num1, num2, num3);
+
+function colores(Red, Green, Blue) {
+  console.log("El color es R:" + Red + ", G: " + Green + ", B:" + Blue);
+}
+
+
+
+
